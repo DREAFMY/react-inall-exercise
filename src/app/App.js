082cadc48ef115bc3ea84eaa303ebe08bc1bddd2
Lu <1625196521@qq.com>
2020-09-24 00:21:29 +0800

    done

--- src/app/App.js
+++ src/app/App.js
@@ -1,14 +1,30 @@
-import React, {Component} from 'react';
+
+import React, { Component } from 'react';
 import './app.less';
-import {Route, BrowserRouter} from "react-router-dom";
+import { Route, BrowserRouter, Link, Switch } from "react-router-dom";
 import Home from "./Home";
+import NotMatch from './component/NotMatch'
+import Calculator from './component/Calculator';
+import Timer from './component/Timer';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
         <BrowserRouter>
-          <Route exact path='/' component={Home}/>
+          <div className="header">
+            <Link className='calculatorLink' to="/timer">在线倒计时器</Link>
+            <Link className='countDowntLink' to='/calculator'>在线计算器</Link>
+            <Link className='homeLink' to="/">Home</Link>
+          </div>
+          <div className="body">
+            <Switch>
+              <Route exact path='/calculator' component={Calculator} />
+              <Route exact path='/timer' component={Timer} />
+              <Route exact path='/' component={Home} />
+              <Route component={NotMatch} />
+            </Switch>
+          </div>
         </BrowserRouter>
       </div>
     );
